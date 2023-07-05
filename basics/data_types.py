34c129ce6ec1d75f55922e830d53a662a55ae3f6
TimaDootaliev <timur.dootaliev.td@gmail.com>
2023-07-05 20:01:32 +0600

    data types and integers

---
+++ basics/data_types.py
@@ -0,0 +1,57 @@
+# Python - высокоуровневый язык, динамически типизированный
+
+# Литерал - обозначение типа данных в коде
+
+# изменяемые
+# dict - словарь - {}
+# list - список - []
+# set - множества - {}
+
+# неизменяемые
+# frozenset - множества - {}
+# int - целые числа - 10
+# float - числа с плавающей точкой - (3.14)
+# complex - сложные числа - (3j)
+# bool - логический тип данных (True/False)
+# str - строки - '', "", """ """
+# tuple - кортеж - (1,)
+# None - обозначение пустоты
+
+# n = 10
+
+n = 10 # Ok
+my_num = 20
+myNum = 30
+
+number = 10
+age = 34 # Ok
+
+x = 2 # Not Ok
+c = 5
+
+# 1my_num = 44 # Error
+# !()$%my_num = 55 # Error
+# _my_num = 66 # Ok
+
+# N = 20
+
+# print(50)
+# print() - функция для вывода информации в терминал
+# input() - функция для ввода информации из терминала
+
+# text = input()
+# print("Text was ", text)
+
+# first_num = int(input("1 "))
+# second_num = int(input("2 "))
+# print(first_num + second_num)
+
+# str_num = "10"
+# int_num = int(str_num)
+# print(type(str_num))
+# print(type(int_num))
+
+
+# str(10) # "10"
+# int("10") # 10
+
